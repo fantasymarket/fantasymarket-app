ff777e34a4ba7582dcdae6880373e85787bdc2e4
Henry <mail@henrygressmann.de>
2020-02-26 15:42:57 +0100

    refactor: refactor stock page
    Signed-off-by: Henry <mail@henrygressmann.de>

--- components/stock/index.js
+++ components/stock/index.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import StockChart from 'components/chart/stock';
-import Button from 'components/ui/button';
 
 import News from './news';
+import Sidebar from './sidebar';
 
 const Wrapper = styled.div`
 	display: flex;
@@ -43,33 +43,6 @@ const ChartTitle = styled.div`
 		}
 	}
 `;
-const SidebarWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	width: 20rem;
-
-	button {
-		margin-bottom: 0.5rem;
-	}
-`;
-
-const Stats = styled.table`
-	border-collapse: collapse;
-	p {
-		margin: 0.2rem 0;
-		font-weight: 600;
-	}
-	td {
-		height: 2rem;
-	}
-	td:first-of-type p {
-		font-weight: 300;
-	}
-	td:last-of-type p {
-		text-align: right;
-		font-weight: 400;
-	}
-`;
 
 const Description = styled.div`
 	display: flex;
@@ -80,61 +53,7 @@ const Description = styled.div`
 	}
 `;
 
-const Performance = styled.div`
-	button {
-		width: 100%;
-	}
-
-	h1 {
-		margin-bottom: 1rem;
-		font-size: 1.4rem;
-
-		b {
-			color: black;
-			padding: 0.2rem 0.2rem 0 0.2rem;
-			border-radius: 4px;
-		}
-
-		span b {
-			font-size: 1.1rem;
-			margin-bottom: 0.5rem;
-		}
-
-		span.down {
-			color: #ff2255;
-		}
-		span.up {
-			color: #22ff8f;
-		}
-		p.left {
-			text-align: left;
-		}
-
-		table {
-			border-collapse: collapse;
-			p {
-				margin: 0.2rem 0;
-				font-weight: 600;
-			}
-			td:last-of-type p {
-				margin-left: 1rem;
-				text-align: left;
-				font-weight: 400;
-			}
-		}
-	}
-	h2.price {
-		font-size: 1.8rem;
-		span {
-			color: white;
-		}
-	}
-	h2 {
-		font-size: 1.2rem;
-	}
-`;
-
-const Stock = ({ symbol }) => {
+const Stock = ({ symbol, about, news }) => {
 	return (
 		<Wrapper>
 			<ChartWrapper>
@@ -152,203 +71,84 @@ const Stock = ({ symbol }) => {
 				<Description>
 					<div>
 						<h1>News</h1>
-						<News
-							news={[
-								{
-									title: 'Google did an oopsie',
-									content: 'Big oof',
-								},
-							]}
-						/>
+						<News news={news} />
 					</div>
 					<div>
 						<h1>About</h1>
 						<div>
-							<p>
-								<b>Alphabet Inc.</b> is an American multinational{' '}
-								<a
-									href="/wiki/Conglomerate_(company)"
-									title="Conglomerate (company)"
-								>
-									conglomerate
-								</a>{' '}
-								headquartered in{' '}
-								<a
-									href="/wiki/Mountain_View,_California"
-									title="Mountain View, California"
-								>
-									Mountain View, California
-								</a>
-								. It was created through a{' '}
-								<a href="/wiki/Restructuring" title="Restructuring">
-									corporate restructuring
-								</a>{' '}
-								of Google on October 2, 2015, and became the parent company of
-								Google and several former Google{' '}
-								<a href="/wiki/Subsidiary" title="Subsidiary">
-									subsidiaries
-								</a>
-								. The two founders of Google assumed executive roles in the new
-								company, with{' '}
-								<a href="/wiki/Larry_Page" title="Larry Page">
-									Larry Page
-								</a>{' '}
-								serving as CEO and{' '}
-								<a href="/wiki/Sergey_Brin" title="Sergey Brin">
-									Sergey Brin
-								</a>{' '}
-								as president. Alphabet is the{' '}
-								<a
-									href="/wiki/List_of_the_largest_information_technology_companies"
-									className="mw-redirect"
-									title="List of the largest information technology companies"
-								>
-									world's fourth-largest technology company
-								</a>{' '}
-								by revenue and one of the{' '}
-								<a
-									href="/wiki/List_of_public_corporations_by_market_capitalization"
-									title="List of public corporations by market capitalization"
-								>
-									world's most valuable companies
-								</a>
-								.
-							</p>
+							<p>{about}</p>
 						</div>
 					</div>
 				</Description>
 			</ChartWrapper>
-			<SidebarWrapper>
-				<Performance>
-					<Button primary>TRADE</Button>
-					<br />
-					<br />
-					<h2 className="price">Change</h2>
-					<h1>
-						<table>
-							<tbody>
-								<tr>
-									<td>
-										<p>1h</p>
-									</td>
-									<td>
-										<p>
-											<span className="up">+2.2%</span>
-										</p>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<p>24h</p>
-									</td>
-									<td>
-										<p>
-											<span className="up">+9.1%</span>
-										</p>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<p>7d</p>
-									</td>
-									<td>
-										<p>
-											<span className="down">-10.1%</span>
-										</p>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-					</h1>
-
-					<h2>Shares Owned</h2>
-					<h1>46 Shares</h1>
-
-					<h2>Average Cost</h2>
-					<h1>$800</h1>
-
-					<h2>Total Cost</h2>
-					<h1>$36,800</h1>
-				</Performance>
-				<br />
-				<h2>Stats</h2>
-				<Stats>
-					<tbody>
-						<tr>
-							<td>
-								<p>Market Cap</p>
-							</td>
-							<td>
-								<p>$900 Mio</p>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<p>Volume (24h)</p>
-							</td>
-							<td>
-								<p>582,709</p>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<p>Previous Close </p>
-							</td>
-							<td>
-								<p>$1,518.15</p>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<p>Open </p>
-							</td>
-							<td>
-								<p>$1,518.15</p>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<p>1y Target Est </p>
-							</td>
-							<td>
-								<p>$1,616.37</p>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<p>Day's Range</p>
-							</td>
-							<td>
-								<p>
-									$1,498.39
-									<br />- $1,512.21
-								</p>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<p>52 Week Range</p>
-							</td>
-							<td>
-								<p>
-									$1,025.00
-									<br />- $1,532.11
-								</p>
-							</td>
-						</tr>
-					</tbody>
-				</Stats>
-			</SidebarWrapper>
+			<Sidebar />
 		</Wrapper>
 	);
 };
 
 Stock.propTypes = {
 	symbol: PropTypes.string,
+	news: PropTypes.any,
+	about: PropTypes.node,
 };
 
 Stock.defaultProps = {
 	symbol: 'GOOG',
+	news: [
+		{
+			title: 'Google did an oopsie',
+			content: 'Big oof',
+		},
+	],
+	about: (
+		<>
+			<b>Alphabet Inc.</b> is an American multinational{' '}
+			<a href="/wiki/Conglomerate_(company)" title="Conglomerate (company)">
+				conglomerate
+			</a>{' '}
+			headquartered in{' '}
+			<a
+				href="/wiki/Mountain_View,_California"
+				title="Mountain View, California"
+			>
+				Mountain View, California
+			</a>
+			. It was created through a{' '}
+			<a href="/wiki/Restructuring" title="Restructuring">
+				corporate restructuring
+			</a>{' '}
+			of Google on October 2, 2015, and became the parent company of Google and
+			several former Google{' '}
+			<a href="/wiki/Subsidiary" title="Subsidiary">
+				subsidiaries
+			</a>
+			. The two founders of Google assumed executive roles in the new company,
+			with{' '}
+			<a href="/wiki/Larry_Page" title="Larry Page">
+				Larry Page
+			</a>{' '}
+			serving as CEO and{' '}
+			<a href="/wiki/Sergey_Brin" title="Sergey Brin">
+				Sergey Brin
+			</a>{' '}
+			as president. Alphabet is the{' '}
+			<a
+				href="/wiki/List_of_the_largest_information_technology_companies"
+				className="mw-redirect"
+				title="List of the largest information technology companies"
+			>
+				world's fourth-largest technology company
+			</a>{' '}
+			by revenue and one of the{' '}
+			<a
+				href="/wiki/List_of_public_corporations_by_market_capitalization"
+				title="List of public corporations by market capitalization"
+			>
+				world's most valuable companies
+			</a>
+			.
+		</>
+	),
 };
 
 export default Stock;
