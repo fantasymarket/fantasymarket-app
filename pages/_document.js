de85b951be31d3a871d6883f3507be63b4959350
exploingcamera <mail@henrygressmann.de>
2020-04-11 16:54:46 +0200

    feat: add custom favicon
    Signed-off-by: exploingcamera <mail@henrygressmann.de>

--- pages/_document.js
+++ pages/_document.js
@@ -19,8 +19,9 @@ class MyDocument extends Document {
 						content="YOUR road to success in the stock market! No signup required, start learning RIGHT AWAY!"
 					/>
 					<link rel="manifest" href="manifest.json" />
-					<meta name="theme-color" content="#22ff8f" />
+					<meta name="theme-color" content="#7bffc5" />
 					<meta name="msapplication-navbutton-color" content="#22ff8f" />
+					<meta name="msapplication-TileColor" content="#00aba9" />
 					<meta
 						name="apple-mobile-web-app-status-bar-style"
 						content="black-translucent"
@@ -30,6 +31,25 @@ class MyDocument extends Document {
 					<meta name="apple-mobile-web-app-capable" content="yes" />
 					<meta name="application-name" content="FantasyMarket" />
 					<meta name="apple-mobile-web-app-title" content="FantasyMarket" />
+
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00fcc9" />
 				</Head>
 				<body>
 					<Main />
